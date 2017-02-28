0a05002adc5162c81e84b9cb40fc1ef0706c71bf
vivekm21 <vivekmehta212@gmail.com>
2017-02-28 14:57:57 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,33 @@
-// Object creation
-var company = new Object();
-company.name = "Facebook";
-company.ceo = new Object();
-company.ceo.firstName = "Mark";
-company.ceo.favColor = "blue";
+// Functions ARE objects
+function multiply(x, y) {
+  return x * y;
+}
+multiply.version = "v.1.0.0";
+console.log(multiply.version);
 
-console.log(company);
-console.log("Company CEO name is: " 
-  + company.ceo.firstName);
 
-console.log(company["name"]);
-var stockPropName = "stock of company";
-company[stockPropName] = 110;
+// Function factory
+function makeMultiplier(multiplier) {
+  var myFunc = function (x) {
+    return multiplier * x;
+  };
 
-console.log("Stock price is: " + 
-  company[stockPropName]);
+  return myFunc;
+}
 
-// Better way: object literal
-var facebook = {
-  name: "Facebook",
-  ceo: {
-    firstName: "Mark",
-    favColor: "blue"
-  },
-  "stock of company": 110
-};
+var multiplyBy3 = makeMultiplier(3);
+console.log(multiplyBy3(10));
+var doubleAll = makeMultiplier(2);
+console.log(doubleAll(100));
 
+
+
+// Passing functions as arguments
+function doOperationOn(x, operation) {
+  return operation(x);
+}
+
+var result = doOperationOn(5, multiplyBy3);
+console.log(result);
+result = doOperationOn(100, doubleAll);
+console.log(result);
