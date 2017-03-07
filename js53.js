5c772d47e1b506ffb4abfdebc0303edea164a244
vivekm21 <vivekmehta212@gmail.com>
2017-03-07 15:29:40 +0530

    Update js53.js

--- js53.js
+++ js53.js
@@ -1,28 +1,34 @@
-// DOM manipulation
-// console.log(document.getElementById("title"));
-// console.log(document instanceof HTMLDocument);
-
+var cnt= 0;
 function sayHello () {
   var name =
    document.getElementById("name").value;
    var message = "<h2>Hello " + name + "!</h2>";
 
-  // document
-  //   .getElementById("content")
-  //   .textContent = message;
-
+ 
   document
     .getElementById("content")
     .innerHTML = message;
 
-  if (name === "student") {
+  if (name === "btechit") {
     var title = 
       document
         .querySelector("#title")
         .textContent;
+    if (cnt==0)
+{
+ 
     title += " & Lovin' it!";
+     cnt=++;
+}
     document
         .querySelector("h1")
         .textContent = title;
   }
+else{
+       cnt=0;
+        var title = "A028"
+       document
+                .querySelector("h1")
+                .textContent = title;
+}
 }
